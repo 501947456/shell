240debb9af47fbba43b3bf6e59d8a3fcf8c8b24a
501947456 <501947456@qq.com>
2018-05-17 17:51:36 +0800

    Update command.sh

--- common/command.sh
+++ common/command.sh
@@ -14,3 +14,28 @@ systemctl reload sshd
 
 shell获取某个时间段的nginx日志内容
 cat web.log | egrep "01/Apr/2014" | sed  -n '/21:31:36/,/21:50:08/p'  
+
+
+mkdir -p  ~/.pip
+cat <<EOF > ~/.pip/pip.conf
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+
+
+#sshkey_deploy
+#!/usr/bin/expect     
+set timeout 30
+set hostip [lindex $argv 0]
+set password [lindex $argv 1]
+spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$hostip
+expect "*assword:"
+send "$password\n"
+expect eof
+
+运行方式
+expect  sshkey_deploy 192.168.1.186 xxxx
+
